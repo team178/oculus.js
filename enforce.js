aa266690596f2fca3e0f2fdf1a37a63f622240de
Farmington Robitics Team 178 <Webmaster@FarmingtonRobitcs.org>
2013-01-10 21:39:32 -0500

    HTTP module for AXIS Camera

--- enforce.js
+++ enforce.js
@@ -1,18 +1,20 @@
-var net = require('net');
+var http = require('http');
+var net  = require('net');
+
 require('js-yaml');
 
 // Import settings file
 var settings = require('./config/server.yaml');
 
 var server = net.createServer(function(c) { //'connection' listener
-  console.log('server connected');
-  c.on('end', function() {
-    console.log('server disconnected');
-  });
-  c.write('hello\r\n');
-  c.pipe(c);
+	console.log('server connected');
+	c.on('end', function() {
+		console.log('server disconnected');
+	});
+	c.write('hello\r\n');
+	c.pipe(c);
 });
 
 server.listen(settings.port, function() { //'listening' listener
-  console.log('server bound');
+	console.log('server bound');
 });
