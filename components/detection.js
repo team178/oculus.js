6984759ce13a3cea84a7e9aa7526296969dd9773
Brandon Cheng <bcheng.gt@gmail.com>
2013-02-06 19:43:14 -0500

    Rectangle score and inner square removal. Output now distincts target placement

--- components/detection.js
+++ components/detection.js
@@ -14,23 +14,45 @@ exports.processImage = function processImage(im, thresholds) {
 
 	contours = im_canny.findContours();
 
+	var targets = [];
+
 	for(i = 0; i < contours.size(); i++) {
 		if (contours.area(i) > thresholds.maxArea) {
-			console.log(contours.boundingRect(i));
 
+			var previous = contours.boundingRect(i);
+
+			// Rename variable for ease of access
+			current = contours.boundingRect(i);
+
+			console.log(current);
+
+			// Remove double targets
+			if (exports.isInside( contours.boundingRect(i), previous) ) {
+				break;
+			}
+
+			// Determine which target we're looking at
+			placement = exports.getRectangleScore(current.width, current.height);
+
+			targets[placement] = exports.getCenter(
+				current.x,
+				current.y,
+				current.width,
+				current.height
+			);
+
+			/* Enable for debugging
 			big.drawContour(contours, i, thresholds.RED);
 			draw.drawBoundingRect(big, contours, i, thresholds.GREEN);
 
 			draw.drawCenter(big, contours, i, thresholds.BLUE, exports.getCenter);
+			*/
 
-			return exports.getCenter(
-				contours.boundingRect(i).x,
-				contours.boundingRect(i).y,
-				contours.boundingRect(i).width,
-				contours.boundingRect(i).height
-			);
+			
 		}
 	}
+
+	return targets;
 
 }
 
@@ -47,4 +69,49 @@ exports.getCenter = function getCenter(x, y, width, height) {
 	var center_x = x + width/2;
 	var center_y = y + height/2;
 	return [center_x, center_y];
+}
+
+/**
+ * Calculate rectangle score of targets
+ *
+ *
+ */
+exports.getRectangleScore = function getRectangleScore(width, height) {
+	var ratio = width / height;
+
+	var top_size = (54 + 8) / (12 + 8);
+	var mid_size = (54 + 8) / (21 + 8);
+	var bot_size = (29 + 8) / (24 + 8);
+
+	var expand = 1.5;
+
+	var top_low = top_size / expand;
+	var top_high = top_size * expand;
+	var mid_low = mid_size / expand;
+	var mid_high = mid_size * expand;
+	var bot_low = bot_size / expand;
+	var bot_high = bot_size * expand;
+
+	if (ratio >= top_low && ratio <= top_high) {
+		return 'top';
+	} else if (ratio >= mid_low && ratio <= mid_high) {
+		return 'middle';
+	} else if ( ratio >= bot_low && ratio <= bot_high ) {
+		return 'bottom';
+	}
+}
+
+/**
+ *
+ *
+ */
+exports.isInside = function isInside(bigger, smaller) {
+	var smaller_topRight = smaller.x + smaller.width;
+	var bigger_topRight = bigger.x + bigger.width;
+
+	if (bigger.x < smaller.x && Bigger_topRight > smaller_topRight) {
+		return true;
+	}
+
+	return false;
 }
--- components/server.js
+++ components/server.js
@@ -19,8 +19,12 @@ function start(settings, detection, thresholds) {
 
 		camera.requestImage(function(data) {
 			cv.readImage(data, function(err, im) {
-				var center = detection.processImage(im, thresholds);
-				res.end('center is at: ' + center[0] + ', ' + center[1]);
+				var targets = detection.processImage(im, thresholds);
+				if (targets != undefined) {
+					exports.respond(res, targets);
+				} else {
+					res.end('No center');
+				}
 			});
 		});
 
@@ -30,6 +34,23 @@ function start(settings, detection, thresholds) {
 		console.log('Server: bound to port ' + settings.port);
 	});
 
+}
+
+exports.respond = function respond(res, targets) {
+	var response = '<style>body { background: #0B2799; color: yellow; } </style>' +
+	
+	'<p>Brandon and Travis son tostados de pan</p>';
+
+	if (targets['top'] != undefined)
+		response = response + 'top: ' + targets['top'][0] + ', ' + targets['top'][1] + '<br />';
+
+	if (targets['middle'] != undefined)
+		response = response + 'middle: ' + targets['middle'][0] + ', ' + targets['middle'][1] + '<br />';
+
+	if (targets['bottom'] != undefined)
+		response = response + 'bottom: ' + targets['bottom'][0] + ', ' + targets['bottom'][1] + '<br />';
+
+	res.end(response);
 }
 
 exports.start = start;
