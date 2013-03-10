a2cebf532b01e5c6d7251942b15e4f4b63553cde
gluxon <bcheng.gt@gmail.com>
2013-03-10 18:02:49 -0400

    y axis is suppose to be inverted, whoops

--- components/analysis.js
+++ components/analysis.js
@@ -12,16 +12,16 @@ exports.chooseTarget = function chooseTarget(targets) {
 }
 
 exports.normalizeValues = function normalizeValues(target, resolution) {
-	console.log(target);
-	console.log(resolution);
-
 	// Create range of 0 to 1
-	var normalized_x = target[0] / resolution[0];
-	var normalized_y = target[1] / resolution[1];
+	var normalized_x = target[0] / resolution.width;
+	var normalized_y = target[1] / resolution.height;
 
 	// Expand to 2 and shift down to -1.
 	normalized_x = (normalized_x * 2) - 1;
 	normalized_y = (normalized_y * 2) - 1;
+
+	// Invert normalized y (so positive is top)
+	normalized_y = -normalized_y;
 
 	// Return values from ranges of -1 to 1
 	return [normalized_x, normalized_y];
